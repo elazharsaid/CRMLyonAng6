6bd644cba0255dcb32c396808f03918b03c87b94
selazhar <selazhar@gmail.com>
2018-09-14 16:23:04 +0200

    last commit

--- src/environments/environement.firebase.ts
+++ src/environments/environement.firebase.ts
@@ -3,12 +3,7 @@ export const environment = {
   production: true,
 
   firebase: {
-    apiKey: 'AIzaSyB6k4K2Vpo4ze7Mq-rNO0mjUp9UixGWpRY',
-    authDomain: 'form-crm-lyon.firebaseapp.com',
-    databaseURL: 'https://form-crm-lyon.firebaseio.com',
-    projectId: 'form-crm-lyon',
-    storageBucket: 'form-crm-lyon.appspot.com',
-    messagingSenderId: '1079918336839'
+
   }
 
 };
